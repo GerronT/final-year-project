79bebe4894f538e272a50466031e22997b4e6cc3
Gerron Tinoy <regnortony@outlook.com>
2019-02-25 18:32:59 +0000

    added camera API

--- app/src/main/java/com/example/volumecalculator/MainActivity.java
+++ app/src/main/java/com/example/volumecalculator/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.volumecalculator;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout) findViewById(R.id.frameLayout);
+
+        //Open Camera
+        camera = android.hardware.Camera.open();
+        showCamera = new ShowCamera(this, camera);
+
+
     }
 }
